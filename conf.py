848acc4e5ea0566cbf09c7376e2573d3ed488170
elbekyuldashev435 <bekijanoblari@gmail.com>
2025-07-09 23:10:15 +0500

    changed conf

--- conf.py
+++ conf.py
@@ -1,9 +1,7 @@
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env.txt')
-load_dotenv(dotenv_path)
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
