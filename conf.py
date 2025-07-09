21306c5cb72586c18850baa0d35c99390d31b3b5
elbekyuldashev435 <bekijanoblari@gmail.com>
2025-07-10 00:47:56 +0500

    .

--- conf.py
+++ conf.py
@@ -1,9 +1,13 @@
 import os
 from dotenv import load_dotenv
 
-
 if not os.getenv("RAILWAY_ENVIRONMENT"):
-    load_dotenv()
+    load_dotenv('.env.txt')
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+if not os.getenv("RAILWAY_ENVIRONMENT"):
+    print("BOT_TOKEN:", BOT_TOKEN)
+    print("OPENAI_API_KEY:", OPENAI_API_KEY)
